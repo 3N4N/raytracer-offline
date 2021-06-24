0cb68b744e25e4fa63197a4e80d19c5f8130abe9
Enan Ajmain <3nan.ajmain@gmail.com>
2021-06-25 03:47:42 +0600

    Add classes for spheres and triangles

--- inc/object.h
+++ inc/object.h
@@ -12,11 +12,36 @@ public:
     int shine; // exponent term of specular component
 
     Object();
+    Object(double, double, double, double, double, double,
+           int, double, double, double, double);
+    Object(vector3d, double, double, double,
+           int, double, double, double, double);
+    Object(double, double, double, int, double, double, double, double);
     ~Object();
     virtual void draw() = 0;
     void setColor(double, double, double);
     void setShine(int);
     void setCoeff(double, double, double, double);
+};
+
+class Sphere: Object {
+public:
+    double radius;
+
+    Sphere(): Object() { }
+    Sphere(double, double, double, double, double, double, double,
+           int, double, double, double, double);
+    Sphere(vector3d, double, double, double, double,
+           int, double, double, double, double);
+};
+
+class Triangle: Object {
+public:
+    vector3d p1, p2, p3;
+
+    Triangle(): Object() { }
+    Triangle(double,double,double,double,double,double,double,double,double,
+             double, double, double, int, double, double, double, double);
 };
 
 
--- src/object.cpp
+++ src/object.cpp
@@ -1,15 +1,38 @@
 #include "object.h"
 
 
-Object::Object()
-{
+Object::Object() { }
 
+Object::Object(double x, double y, double z,
+               double r, double g, double b,
+               int shine, double ambient, double diffuse,
+               double specular, double recursive)
+{
+    ref_point.x = x;
+    ref_point.y = y;
+    ref_point.z = z;
+
+    setColor(r, g, b);
+    setShine(shine);
+    setCoeff(ambient, diffuse, specular, recursive);
 }
 
-Object::~Object()
-{
+Object::Object(vector3d ref, double r, double g, double b,
+               int shine, double ambient, double diffuse,
+               double specular, double recursive)
+    : Object(ref.x, ref.y, ref.z, r, g, b,
+             shine, ambient, diffuse, specular, recursive) { }
 
+Object::Object(double r, double g, double b,
+               int shine, double ambient, double diffuse,
+               double specular, double recursive)
+{
+    setColor(r, g, b);
+    setShine(shine);
+    setCoeff(ambient, diffuse, specular, recursive);
 }
+
+Object::~Object() { }
 
 void Object::setColor(double r, double g, double b)
 {
@@ -30,4 +53,37 @@ void Object::setCoeff(double ambient, double diffuse,
     coeff[1] = diffuse;
     coeff[2] = specular;
     coeff[3] = reflection;
+}
+
+
+
+Sphere::Sphere(double x, double y, double z, double radius,
+               double r, double g, double b,
+               int shine, double ambient, double diffuse,
+               double specular, double recursive)
+    :
+    Object(x, y, z, r, g, b, shine, ambient, diffuse, specular, recursive),
+    radius(radius) { }
+
+Sphere::Sphere(vector3d center, double radius,
+               double r, double g, double b,
+               int shine, double ambient, double diffuse,
+               double specular, double recursive)
+    :
+    Sphere(center.x, center.y, center.z, radius, r, g, b,
+           shine, ambient, diffuse, specular, recursive) { }
+
+
+
+Triangle::Triangle(double x1, double y1, double z1,
+         double x2, double y2, double z2,
+         double x3, double y3, double z3,
+         double r, double g, double b,
+         int shine, double ambient, double diffuse,
+         double specular, double recursive)
+    : Object(r, g, b, shine, ambient, diffuse, specular, recursive)
+{
+    p1.x = x1; p1.y = y1; p1.z = z1;
+    p2.x = x2; p2.y = y2; p2.z = z2;
+    p3.x = x3; p3.y = y3; p3.z = z3;
 }
