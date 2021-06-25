eaca8949abcf86ce7c0d17ff9682e07001fa0f7d
Enan Ajmain <3nan.ajmain@gmail.com>
2021-06-25 10:52:53 +0600

    Fix: class declarations

--- inc/object.h
+++ inc/object.h
@@ -12,36 +12,49 @@ public:
     int shine; // exponent term of specular component
 
     Object();
-    Object(double, double, double, double, double, double,
-           int, double, double, double, double);
-    Object(vector3d, double, double, double,
-           int, double, double, double, double);
-    Object(double, double, double, int, double, double, double, double);
+    Object(double x, double y, double z,
+           double r, double g, double b,
+           int shine, double ambient, double diffuse,
+           double specular, double recursive);
+    Object(vector3d ref, double r, double g, double b,
+           int shine, double ambient, double diffuse,
+           double specular, double recursive);
+    Object(double r, double g, double b,
+           int shine, double ambient, double diffuse,
+           double specular, double recursive);
     ~Object();
-    virtual void draw() = 0;
+    // virtual void draw() = 0;
     void setColor(double, double, double);
     void setShine(int);
     void setCoeff(double, double, double, double);
 };
 
-class Sphere: Object {
+class Sphere: public Object {
 public:
     double radius;
 
     Sphere(): Object() { }
-    Sphere(double, double, double, double, double, double, double,
-           int, double, double, double, double);
-    Sphere(vector3d, double, double, double, double,
-           int, double, double, double, double);
+    Sphere(double x, double y, double z, double radius,
+           double r, double g, double b,
+           int shine, double ambient, double diffuse,
+           double specular, double recursive);
+    Sphere(vector3d center, double radius,
+           double r, double g, double b,
+           int shine, double ambient, double diffuse,
+           double specular, double recursive);
 };
 
-class Triangle: Object {
+class Triangle: public Object {
 public:
     vector3d p1, p2, p3;
 
     Triangle(): Object() { }
-    Triangle(double,double,double,double,double,double,double,double,double,
-             double, double, double, int, double, double, double, double);
+    Triangle(double x1, double y1, double z1,
+             double x2, double y2, double z2,
+             double x3, double y3, double z3,
+             double r, double g, double b,
+             int shine, double ambient, double diffuse,
+             double specular, double recursive);
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <GL/glut.h>
 
 #include "object.h"
 
+
+
+using namespace std;
+
+
+
 int recursion_depth;
 int screen_size;
 int n_obj;
+vector<Object *> objects;
 
-using namespace std;
+
 
 void loadData()
 {
@@ -18,6 +26,8 @@ void loadData()
     scenefile >> recursion_depth >> screen_size;
     scenefile >> n_obj;
     cout << recursion_depth <<" "<< screen_size <<" "<< n_obj <<"\n";
+
+    Object *obj;
 
     while (n_obj--) {
         string objname;
@@ -33,6 +43,10 @@ void loadData()
             scenefile >> R >> G >> B;
             scenefile >> ambient >> diffuse >> specular >> recursive;
             scenefile >> shine;
+
+            obj = new Sphere(x,y,z, radius, R,G,B, shine,
+                             ambient,diffuse,specular,recursive);
+            objects.push_back(obj);
         }
         else if (objname == "triangle") {
             double x1, y1, z1;
@@ -48,6 +62,10 @@ void loadData()
             scenefile >> R >> G >> B;
             scenefile >> ambient >> diffuse >> specular >> recursive;
             scenefile >> shine;
+
+            obj = new Triangle(x1,y1,z1, x2,y2,z2, x3,y3,z3, R,G,B, shine,
+                               ambient,diffuse,specular,recursive);
+            objects.push_back(obj);
         }
         else if (objname == "general") {
             double a, b, c, d, e, f, g, h, i, j;
