c66496be4b2ecb0d6ba618fdefd864730627dbb9
Enan Ajmain <3nan.ajmain@gmail.com>
2021-06-25 14:19:46 +0600

    Move function definitions for Light in header file

--- inc/light.h
+++ inc/light.h
@@ -16,6 +16,25 @@ public:
     void setColor(double r, double g, double b);
 };
 
+Light::Light(double x, double y, double z,
+             double r, double g, double b)
+{
+    setPosition(x, y, z);
+    setColor(r, g, b);
+}
+
+void Light::setPosition(double x, double y, double z)
+{
+    this->pos.x = x;
+    this->pos.y = y;
+    this->pos.z = z;
+}
+
+void Light::setColor(double r, double g, double b)
+{
+    color[0] = r;
+    color[1] = g;
+    color[2] = b;
+}
 
 #endif // _LIGHT_H_
-
